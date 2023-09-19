8170f297afe6d8b90653c46f9ac06ba6c8ff3067
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2023-09-19 23:50:26 +0000

    Added buttons to hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,22 +10,19 @@ const HeroSection = () => {
             <p>
                 Hello, I'm David Haastrup, a Fullstack Web Developer with a passion for crafting digital solutions using JavaScript. While I may be at the beginning of my professional journey, my enthusiasm for web development knows no bounds.
             </p>
-
-            {/* <h2>About me</h2>
-            <div className="blocks">
-                <div className="block">
-                    <h3>Passion for Learning</h3>
-                    <p>
-                        One thing that defines me is my unwavering passion for learning. In this ever-evolving field, I thrive on staying up-to-date with the latest technologies, frameworks, and best practices. 
-                    </p>
-                </div>
-                <div className="block">
-                    <h3>Creating and Problem-Solving</h3>
-                    <p>
-                        I find immense joy in creating new things and solving intricate problems. 
-                    </p>
-                </div>
-            </div> */}
+            <div className="buttons">
+                <a href="" 
+                    target='_blank' rel="noreferrer"
+                >
+                    View Linkedin
+                </a>
+                <a href="https://github.com/DavidFlutter" 
+                    target='_blank' rel="noreferrer"
+                    className='alt'
+                >
+                    View Github
+                </a>
+            </div>
         </div>
         <div className="hero-image">
             <img src={require("../images/hero_image.jpg")} alt="" />
