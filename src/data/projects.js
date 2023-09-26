454dda7b0848c09ecfb2fa6002e3d223ad2c8164
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2023-09-26 19:22:56 +0000

    Added roboart1.0 project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,68 +2,27 @@ const projects = [
     {
         id: 1,
         name: "Fitness website",
-        link: "", 
+        link: "https://davidflutter.github.io/fitness-website/", 
         desc: "The frontend for a fitness company, built with ReactJS and SCSS, where user can view details of the gym, trainers, subsription plans, and the join in a get fit ", 
         github: "https://github.com/DavidFlutter/fitness-website.git",
         image: require("../images/fitness_website_image.png")
     },
     {
         id: 2,
+        name: "Robo Art",
+        link: "https://davidflutter.github.io/robo_art1.0/",
+        desc: "A clone of the frontend mentor landing page built with ReactJS, SCSS, and External APIs", 
+        github:"https://github.com/DavidFlutter/robo_art1.0.git",
+        image: require("../images/robo_art_image.png")
+    },
+    {
+        id: 3,
         name: "Frontend Mentor clone",
-        link: "",
+        link: "https://davidflutter.github.io/frontend-mentor-clone/",
         desc: "A clone of the frontend mentor landing page built with ReactJS and SCSS", 
         github:"https://github.com/DavidFlutter/frontend-mentor-clone.git",
         image: require("../images/frontend_mentor_clone_image.png")
     },
-    {
-        id: 1,
-        name: "Fitness website",
-        link: "", 
-        desc: "The frontend for a fitness company where user can view details of the gym, trainers, subsription plans, and the join in a get fit", 
-        github: "https://github.com/DavidFlutter/fitness-website.git",
-        image: require("../images/fitness_website_image.png")
-    },
-    {
-        id: 2,
-        name: "Frontend Mentor clone",
-        link: "",
-        desc: "A clone of the frontend mentor landing page", 
-        github:"https://github.com/DavidFlutter/frontend-mentor-clone.git",
-        image: require("../images/frontend_mentor_clone_image.png")
-    },
-    {
-        id: 1,
-        name: "Fitness website",
-        link: "", 
-        desc: "The frontend for a fitness company where user can view details of the gym, trainers, subsription plans, and the join in a get fit", 
-        github: "https://github.com/DavidFlutter/fitness-website.git",
-        image: require("../images/fitness_website_image.png")
-    },
-    {
-        id: 2,
-        name: "Frontend Mentor clone",
-        link: "",
-        desc: "A clone of the frontend mentor landing page", 
-        github:"https://github.com/DavidFlutter/frontend-mentor-clone.git",
-        image: require("../images/frontend_mentor_clone_image.png")
-    },
-    {
-        id: 1,
-        name: "Fitness website",
-        link: "", 
-        desc: "The frontend for a fitness company where user can view details of the gym, trainers, subsription plans, and the join in a get fit", 
-        github: "https://github.com/DavidFlutter/fitness-website.git",
-        image: require("../images/fitness_website_image.png")
-    },
-    {
-        id: 2,
-        name: "Frontend Mentor clone",
-        link: "",
-        desc: "A clone of the frontend mentor landing page", 
-        github:"https://github.com/DavidFlutter/frontend-mentor-clone.git",
-        image: require("../images/frontend_mentor_clone_image.png")
-    },
-    
 ]
 
 
