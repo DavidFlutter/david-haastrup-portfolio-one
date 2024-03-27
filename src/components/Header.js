24082674307dc3be9f920d48535c85aec2aa959e
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2024-03-27 13:58:33 +0000

    c sharp redesign

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='Header'>
-        <h1>David The Fullstack Dev</h1>
+        <h1>David The Developer</h1>
         <nav>
             <ul>
                 <a href="#HeroSection">About</a>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,10 +5,10 @@ const HeroSection = () => {
     <div className='HeroSection' id='HeroSection'>
         <div className="hero-text">
             <h2>
-                Hi there I'm David, a Fullstack <span>JS</span> developer based in Nigeria
+                Hi there I'm David, an up and coming <span>C#</span> Back-End developer
             </h2>
             <p>
-                Hello, I'm David Haastrup, a Fullstack Web Developer with a passion for crafting digital solutions using JavaScript. While I may be at the beginning of my professional journey, my enthusiasm for web development knows no bounds.
+                Hello, I'm David Haastrup, an up and coming C# Back-End Developer with a passion for crafting solutions using C#. While I may be at the beginning of my software development journey, my enthusiasm for software development knows no bounds <br />I am also skill at <b>frontend</b> development using the <b>React</b>
             </p>
             <div className="buttons">
                 <a href="https://www.linkedin.com/in/david-haastrup-678609209/" 
