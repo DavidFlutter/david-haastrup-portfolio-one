39dc560f3e184a0a21bcc6d3d74cfd1e5f8eb105
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2024-03-27 13:39:03 +0000

    Add new projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,22 @@
 const projects = [
     {
         id: 1,
+        name: "Scholard AI 1.0",
+        link: "https://davidflutter.github.io/scholard-frontend-1.0/", 
+        desc: "The frontend for Scholard AI 1.0, built with ReactJS and SCSS", 
+        github: "https://github.com/DavidFlutter/scholard-frontend-1.0.git",
+        image: require("../images/scholard_ai_1.png")
+    },
+    {
+        id: 2,
+        name: "Scholard AI 2.0",
+        link: "https://davidflutter.github.io/scholard-frontend2.0/", 
+        desc: "The frontend for Scholard AI 2.0, built with ReactJS and SCSS",
+        github: "https://github.com/DavidFlutter/scholard-frontend2.0.git",
+        image: require("../images/scholard_ai_2.png")
+    },
+    {
+        id: 3,
         name: "Fitness website",
         link: "https://davidflutter.github.io/fitness-website/", 
         desc: "The frontend for a fitness company, built with ReactJS and SCSS, where user can view details of the gym, trainers, subsription plans, and the join in a get fit ", 
@@ -8,7 +24,7 @@ const projects = [
         image: require("../images/fitness_website_image.png")
     },
     {
-        id: 2,
+        id: 4,
         name: "Robo Art",
         link: "https://davidflutter.github.io/robo_art1.0/",
         desc: "A clone of the frontend mentor landing page built with ReactJS, SCSS, and External APIs", 
@@ -16,7 +32,7 @@ const projects = [
         image: require("../images/robo_art_image.png")
     },
     {
-        id: 3,
+        id: 5,
         name: "Frontend Mentor clone",
         link: "https://davidflutter.github.io/frontend-mentor-clone/",
         desc: "A clone of the frontend mentor landing page built with ReactJS and SCSS", 
